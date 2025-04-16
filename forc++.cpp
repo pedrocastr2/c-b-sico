252d5970dcf7ada9151ca5d63f649a9213ede16d
pedrocastr <102086416+pedrocastr2@users.noreply.github.com>
2025-04-15 23:10:50 -0300

    Add files via upload

---
+++ forc++.cpp
@@ -0,0 +1,18 @@
+#include <iostream>  // Biblioteca para entrada e saída
+using namespace std; // Evita ter que escrever std:: antes de cout e cin
+
+
+int main() {
+	int x;
+	x = 0;
+	
+	for (x - 0;x < 10;x++) {
+		cout << "\n" << x;
+		
+	}
+
+
+	
+
+	return 0; // Indica que o programa terminou com sucesso
+}
